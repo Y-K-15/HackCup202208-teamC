29999bc63584882d24aa8bab7243b300b1182210
ko-shiro-hap <90499315+ko-shiro-hap@users.noreply.github.com>
2022-08-18 20:19:39 +0900

    問題配列作成、正誤判定によるスコア表示判定によるスコア表示機能作成

--- js/game2.js
+++ js/game2.js
@@ -1,9 +1,44 @@
 'use strict';
 
+const questions = [
+  {
+    word: '７つの習慣',
+    alphabet: 'nanatunosyuukann'
+  },
+  {
+    word: 'パラダイム',
+    alphabet: 'paradaimu'
+  },
+  {
+    word: 'インサイドアウト',
+    alphabet: 'insaidoauto'
+  },
+  {
+    word: 'ガチョウの卵',
+    alphabet: 'gatyounotamago'
+  },
+  {
+    word: '影響の輪',
+    alphabet: 'eikyounowa'
+  },
+  {
+    word: '相互依存',
+    alphabet: 'sougoizonn'
+  },
+  {
+    word: 'パーソナルマネジメント',
+    alphabet: 'pa-sonarumanezimento'
+  },
+]
+
 const RANDOM_SENTENCE_URL_API = 'https://api.quotable.io/random';
 const typeDisplay = document.getElementById('type-display');
 const typeInput = document.getElementById('type-input');
 const timer = document.getElementById('timer');
+const resultDisplay = document.getElementById('result-display');
+const defaultQuestionLength = questions.length;
+let correctCount = 0;
+let sentence;
 
 
 // inputテキスト入力。正誤判定
@@ -23,18 +58,23 @@ typeInput.addEventListener('input', () => {
       characterSpan.classList.remove("correct");
     }
   })
-})
 
-// 非同期処理でランダムな文章を取得する
-function GetRandomSentence() {
-  return fetch(RANDOM_SENTENCE_URL_API)
-  .then((response) => response.json())
-  .then((data) => data.content);
-}
+  if (typeInput.value == sentence) {
+    console.log(sentence);
+    correctCount++;
+    if (questions.length != 0) {
+      RenderNextSentence();
+    } else {
+      showResults();
+    }
+  }
+})
 
 // ランダムな文章を取得して、表示する
 async function RenderNextSentence() {
-  const sentence = await GetRandomSentence();
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  sentence = questions[randomIndex].alphabet;
+  typeDisplay.innerHTML = questions[randomIndex].word + "<br>";
 
   // 文章を1文字ずつ分解して、spanタグを生成する
   let oneText = sentence.split("");
@@ -44,19 +84,26 @@ async function RenderNextSentence() {
     typeDisplay.appendChild(characterSpan);
   });
 
+  // 配列から問題を消す
+  questions.splice(randomIndex, 1)
+
   // テキストボックスの中身を消す
   typeInput.value = "";
   startTimer();
 }
 
 let startTime;
-let originTime = 30;
+let originTime = 5;
 function startTimer() {
   timer.innerHTML = originTime;
   startTime = new Date();
   setInterval(() => {
-    timer.innerHTML = originTime - getTimerTime();
-    if(timer.innerHTML <= 0) timeUp();
+    if(questions.length == 0 && timer.innerHTML <= 0){
+      clearInterval();
+    } else {
+      timer.innerHTML = originTime - getTimerTime();
+      if(timer.innerHTML <= 0) timeUp();
+    }
   }, 1000);
 }
 
@@ -65,8 +112,23 @@ function getTimerTime() {
 }
 
 function timeUp() {
-  typeDisplay.innerHTML = "";
-  RenderNextSentence();
+  if(questions.length != 0){
+    typeDisplay.innerHTML = "";
+    RenderNextSentence();
+  } else {
+    console.log("finish");
+    showResults();
+  }
+}
+
+function showResults() {
+  timer.classList.add("js-off");
+  typeDisplay.classList.add("js-off");
+  typeInput.classList.add("js-off");
+
+  resultDisplay.innerHTML = correctCount + "/" + defaultQuestionLength;
+  resultDisplay.classList.remove("js-off");
 }
 
 RenderNextSentence();
+
